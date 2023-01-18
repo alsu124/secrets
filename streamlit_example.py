d157179222081f30a31d7a905be4cc5f516c6d35
Alsu <58417160+alsu124@users.noreply.github.com>
2023-01-18 14:47:41 +0300

    Add files via upload

--- streamlit_example.py
+++ streamlit_example.py
@@ -2,32 +2,21 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import streamlit as st
-#from google.oauth2 import service_account
+
+from shillelagh.backends.apsw.db import connect
+
 from gsheetsdb import connect
 
-# Create a connection object.
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"],
-    scopes=[
-        "https://www.googleapis.com/auth/spreadsheets",
-    ],
-)
-conn = connect(credentials=credentials)
-# Perform SQL query on the Google Sheet.
-# Uses st.cache to only rerun when the query changes or after 10 min.
-@st.cache(ttl=600)
-def run_query(query):
-    rows = conn.execute(query, headers=1)
-    rows = rows.fetchall()
-    return rows
-
-sheet_url = st.secrets["private_gsheets_url"]
-rows = run_query(f'SELECT * FROM "{sheet_url}"')
-
-# Print results.
-for row in rows:
-    st.write(f"{row.name} has a :{row.pet}:")
-
+conn = connect()
+result = conn.execute("""
+    SELECT
+        *
+    FROM
+        "https://docs.google.com/spreadsheets/d/1Y74Bc8H2fJvhFobbhesPESMouNpG8hRiwggsjRavg_w/edit?usp=drive_web&ouid=102148399879679509805"
+    
+""", headers=1)
+for row in result:
+    print(row)
 
 
 
